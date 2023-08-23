42e0e0611ffb0134180b75da02504bb04e985218
adamasultan <112110114+adamasultan@users.noreply.github.com>
2023-08-22 23:05:38 -0400

    Refactor
    * Refactor
    
    * Old game (working)
    
    * mouse handling on grid
    
    * Done refactor
    
    * renaming private funcs
    
    * delete old files

--- src/game.py
+++ src/game.py
@@ -1,179 +1,31 @@
-from grid import Grid
-import copy
-import pygame
-
+from gamelogic import GameLogic
+from gui import Gui
+from gamestate import GameState
 class Game():
-    def __init__(self, grid = Grid()):
-        self.grid = grid
+    def __init__(self, width, height, cell_size):
+        grid_width = width // cell_size
+        grid_height = height // cell_size
+        self.game_logic = GameLogic(grid_width, grid_height)
+        self.display = Gui(self.game_logic,width, height, cell_size, grid_width, grid_height, self.on_exit_press, self.on_cell_press, self.on_next_gen_press)
+        self.running = False
 
-    def sequence(self):
-        def count_alives(row, col, alives = 0) -> int:
-            if col == 0 and row == 0:
-                if self.grid.state[row+1][col]:
-                    alives+=1
-                
-                if self.grid.state[row][col+1]:
-                    alives+=1
-                
-                if self.grid.state[row+1][col+1]:
-                    alives+=1
-                
+    def start_game(self):
+        self.running = True
+        while self.running:
+            self.display.handle_user_input()
+            self.display.update()
+        self.display.quit()
+    def on_next_gen_press(self):
+        self.game_logic.sequence()
+        self.display.update_grid()
 
-            elif col == 0 and row == self.grid.grid_height-1:
-                if self.grid.state[row-1][col]:
-                    alives+=1
-                
-                if self.grid.state[row][col+1]:
-                    alives+=1
-                
-                if self.grid.state[row-1][col+1]:
-                    alives+=1
-                
-                    
-            elif col == self.grid.grid_width-1 and row == 0:
-                if self.grid.state[row+1][col]:
-                    alives+=1
-                
-                if self.grid.state[row][col-1]:
-                    alives+=1
-                
-                if self.grid.state[row+1][col-1]:
-                    alives+=1
-                
+    def on_exit_press(self):
+        self.running = False
+    def on_cell_press(self, *args):
+        row, col = args
+        self.game_logic.change_state(row, col)
+        self.display.update_cell(row,col)
+        #print(self.game_logic.state[row][col])
 
-            elif col == self.grid.grid_width-1 and row == self.grid.grid_height-1:
-                if self.grid.state[row-1][col]:
-                    alives+=1
-                
-                if self.grid.state[row][col-1]:
-                    alives+=1
-                
-                if self.grid.state[row-1][col-1]:
-                    alives+=1
-                
-                        # note to future, this is chekcing for squares ar col 0
-            elif col == 0:
-                if self.grid.state[row+1][col]:
-                    alives+=1
-                
-                if self.grid.state[row-1][col]:
-                    alives+=1
-                
-                if self.grid.state[row][col+1]:
-                    alives+=1
-                
-                if self.grid.state[row+1][col+1]:
-                    alives+=1
-                
-                if self.grid.state[row-1][col+1]:
-                    alives+=1
-                
-            elif col == self.grid.grid_width -1:
-                if self.grid.state[row+1][col]:
-                    alives+=1
-                
-                if self.grid.state[row-1][col]:
-                    alives+=1
-                
-                if self.grid.state[row][col-1]:
-                    alives+=1
-                
-                if self.grid.state[row+1][col-1]:
-                    alives+=1
-                
-                if self.grid.state[row-1][col-1]:
-                    alives+=1
-                
-            elif row == 0:
-                if self.grid.state[row+1][col]:
-                    alives+=1
-                
-                if self.grid.state[row][col-1]:
-                    alives+=1
-                
-                if self.grid.state[row+1][col-1]:
-                    alives+=1
-                
-                if self.grid.state[row][col+1]:
-                    alives+=1
-                
-                if self.grid.state[row+1][col+1]:
-                    alives+=1
-                
-            elif row == self.grid.grid_height -1:
-                if self.grid.state[row-1][col]:
-                    alives+=1
-                
-                if self.grid.state[row][col-1]:
-                    alives+=1
-                
-                if self.grid.state[row-1][col-1]:
-                    alives+=1
-                
-                if self.grid.state[row][col+1]:
-                    alives+=1
-                
-                if self.grid.state[row-1][col+1]:
-                    alives+=1
-                
-            #start of general surrounding comparison
-            else:
-                if self.grid.state[row+1][col]:
-                    alives+=1
-                
-                if self.grid.state[row-1][col]:
-                    alives+=1
-                
-                if self.grid.state[row][col-1]:
-                    alives+=1
-                
-                if self.grid.state[row+1][col-1]:
-                    alives+=1
-                
-                if self.grid.state[row-1][col-1]:
-                    alives+=1
-                
-                if self.grid.state[row][col+1]:
-                    alives+=1
-                
-                if self.grid.state[row+1][col+1]:
-                    alives+=1
-                
-                if self.grid.state[row-1][col+1]:
-                    alives+=1
-                
-            return alives
-        next = copy.deepcopy(self.grid.state)
-        #print(self.grid.grid)
-        for row in range(self.grid.grid_height):
-            for col in range(self.grid.grid_width):
-                # print(self.grid.state[0][0])
-                alives = count_alives(row,col)
-                #print(f"alive:{alives}  dead:{deads}")
-                #print(self.grid.state[row][col])
-                #print(self.grid.state[0][0])
-                if self.grid.state[row][col]:
-                    if alives < 2:
-                        next[row][col] = 0
-                    elif alives > 3:
-                        next[row][col] = 0
-                elif self.grid.state[row][col] == 0:
-                    if alives == 3:
-                        next[row][col] = 1
-                #print(self.grid.state[row][col])
-        self.grid.state = copy.deepcopy(next)
-        #return self.grid.grid
-
-        #print(next)
-
-                
-    
-    
-
-
-game = Game()
-game.grid.create_grid(game)
-#print(game.grid.grid)
-#game.sequence()
-#print(game.grid.grid)
-#game.game()
+game = Game(800, 600, 40)
+game.start_game()
---
+++ src/gui.py
@@ -0,0 +1,68 @@
+import pygame
+class Gui():
+    def __init__(self, game_logic, width, height, cell_size, grid_width, grid_height, on_exit_press, on_cell_press, on_next_gen_press):
+        self.game_logic = game_logic
+        self.width=  width
+        self.height = height
+        self.cell_size = cell_size
+        self.grid_width = grid_width
+        self.grid_height = grid_height
+        self.on_exit_press = on_exit_press
+        self.on_cell_press = on_cell_press
+        self.on_next_gen_press = on_next_gen_press
+        self.RED = (255, 64, 64)
+        self.BLACK = (0, 0, 0)
+        self.GREEN = (0, 100, 0)
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.init()
+        pygame.display.set_caption("Conway's Game of Life")
+        self.screen.fill(self.RED)
+        self.__draw_grid(self.BLACK)
+
+    def __draw_rect(self,color, row, col):
+        pygame.draw.rect(self.screen, color, (col*self.cell_size, row*self.cell_size, self.cell_size, self.cell_size))
+
+    def __draw_grid(self, color):
+        for row in range(self.grid_height):
+            pygame.draw.lines(self.screen, color, True, ((0, row*self.cell_size), (self.width, row*self.cell_size)),1)
+        for col in range(self.grid_width):
+            pygame.draw.lines(self.screen, color, True, ((col*self.cell_size, 0), (col*self.cell_size, self.height)),1)
+
+    def update_grid(self):
+        #self.screen.fill(self.RED)
+        for row in range(self.grid_height):
+            for col in range(self.grid_width):
+                if self.game_logic.is_alive(row,col):
+                    self.__draw_rect(self.GREEN, row, col)
+                else:
+                    self.__draw_rect(self.RED, row, col)
+
+    def update_cell(self, row, col):
+        if self.game_logic.is_alive(row,col):
+            self.__draw_rect(self.GREEN, row, col)
+        else:
+            self.__draw_rect(self.RED, row, col)
+
+    def handle_user_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.on_exit_press()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                    # find each row and column that has that specific mouse click
+                    # finds specific squqare
+                row = y//self.cell_size
+                col = x//self.cell_size
+                self.on_cell_press(row, col)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    self.on_next_gen_press()
+                
+    def update(self):
+
+        self.__draw_grid(self.BLACK)
+        pygame.display.flip()
+    
+    
+    def quit(self):
+        pygame.quit()
