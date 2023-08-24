441b337ef97cc9e5bb5ca3e0ca09fbdb432499df
adamasultan <112110114+adamasultan@users.noreply.github.com>
2023-08-24 15:40:52 -0400

    Addbuttons (#2)
    * Added bottom area to add buttons
    
    * only using is_alive in game logic
    
    * added area near bottom for space for buttons
    
    * created templates for buttons
    
    * added function to star,stop,reset and next buttons
    
    * start,stop,reset,next work
    
    * all buttons work ready for merge

--- src/game.py
+++ src/game.py
@@ -2,16 +2,17 @@ from gamelogic import GameLogic
 from gui import Gui
 class Game():
     def __init__(self, width = 800, height = 600, cell_size = 40):
-        grid_width = width // cell_size
-        grid_height = height // cell_size
-        self.game_logic = GameLogic(grid_width, grid_height)
-        self.display = Gui(self.game_logic,width, height, cell_size, grid_width, grid_height, self.on_exit_press, self.on_cell_press, self.on_next_gen_press)
+        self.grid_width = width // cell_size
+        self.grid_height = height // cell_size
+        self.game_logic = GameLogic(self.grid_width, self.grid_height)
+        self.display = Gui(self.game_logic,width, height, cell_size, self.grid_width, self.grid_height, self.on_exit_press, self.on_cell_press, self.on_next_gen_press, self.on_reset)
         self.running = False
 
     def start_game(self):
         self.running = True
         while self.running:
             self.display.handle_user_input()
+
             self.display.update()
         self.display.quit()
     def on_next_gen_press(self):
@@ -25,6 +26,8 @@ class Game():
         self.game_logic.change_state(row, col)
         self.display.update_cell(row,col)
         #print(self.game_logic.state[row][col])
-
+    def on_reset(self):
+        self.game_logic.state = [[0 for _ in range(self.grid_width)] for _ in range(self.grid_height)]
+        self.display.reset_grid()
 game = Game()
 game.start_game()
--- src/gui.py
+++ src/gui.py
@@ -1,6 +1,7 @@
 import pygame
+import time
 class Gui():
-    def __init__(self, game_logic, width, height, cell_size, grid_width, grid_height, on_exit_press, on_cell_press, on_next_gen_press):
+    def __init__(self, game_logic, width, height, cell_size, grid_width, grid_height, on_exit_press, on_cell_press, on_next_gen_press, on_reset):
         self.game_logic = game_logic
         self.width=  width
         self.height = height
@@ -10,24 +11,54 @@ class Gui():
         self.on_exit_press = on_exit_press
         self.on_cell_press = on_cell_press
         self.on_next_gen_press = on_next_gen_press
+        self.on_reset = on_reset
         self.RED = (255, 64, 64)
         self.BLACK = (0, 0, 0)
         self.GREEN = (0, 100, 0)
-        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.GREY = (169, 169, 169)
+        self.screen = pygame.display.set_mode((self.width, self.height+self.height*0.25))
         pygame.init()
+        self.start_text_font = pygame.font.SysFont("Arial", int(self.cell_size))
+        self.reset_text_font = pygame.font.SysFont("Arial", int(0.75*self.cell_size))
+        self.stop_and_next_text_font = pygame.font.SysFont("Arial", int(0.55*self.cell_size))
         pygame.display.set_caption("Conway's Game of Life")
         self.screen.fill(self.RED)
         self.__draw_grid(self.BLACK)
+        pygame.draw.rect(self.screen, self.GREY, ((0,self.height),(self.width, self.height)))
+        self.__draw_buttons()
 
     def __draw_rect(self,color, row, col):
         pygame.draw.rect(self.screen, color, (col*self.cell_size, row*self.cell_size, self.cell_size, self.cell_size))
 
+    def __draw_buttons(self):
+        pygame.draw.rect(self.screen, self.GREEN, ((0.4*self.width, self.height+self.height*0.05), (4*self.cell_size, 2*self.cell_size)))
+        self.draw_text("Start", self.start_text_font, (0,0,0), 0.46*self.width, self.height+self.height*0.08)
+        pygame.draw.rect(self.screen, self.GREEN, ((0.15*self.width, self.height+self.height*0.075), (2.5*self.cell_size, 1.5*self.cell_size)))
+        self.draw_text("Reset", self.reset_text_font, (0,0,0), 0.17*self.width, self.height+self.height*0.09)
+        pygame.draw.rect(self.screen, self.RED, ((0.65*self.width, self.height+self.height*0.1), (2*self.cell_size, self.cell_size)))
+        self.draw_text("Stop", self.stop_and_next_text_font, (0,0,0), 0.68*self.width, self.height+self.height*0.11)
+        pygame.draw.rect(self.screen, self.GREEN, ((0.82*self.width, self.height+self.height*0.1), (2*self.cell_size, self.cell_size)))
+        self.draw_text("Next", self.stop_and_next_text_font, (0,0,0), 0.85*self.width, self.height+self.height*0.11)
     def __draw_grid(self, color):
-        for row in range(self.grid_height):
+        for row in range(self.grid_height+1):
             pygame.draw.lines(self.screen, color, True, ((0, row*self.cell_size), (self.width, row*self.cell_size)),1)
-        for col in range(self.grid_width):
+        for col in range(self.grid_width+1):
             pygame.draw.lines(self.screen, color, True, ((col*self.cell_size, 0), (col*self.cell_size, self.height)),1)
-
+    
+    def __in_grid_range(self,x,y):
+        return x <= self.width and y<= self.height  
+    def __in_start_button_range(self,x,y):
+        return x>= 0.4*self.width and x <= 0.4*self.width+4*self.cell_size and y>= self.height+self.height*0.05 and y <= self.height+self.height*0.05+2*self.cell_size
+    def __in_stop_button_range(self,x,y):
+        return x>=0.65*self.width and x<=0.65*self.width+2*self.cell_size and y>=self.height+self.height*0.1 and y<=self.height+self.height*0.1 + self.cell_size
+    def __in_reset_button_range(self,x,y):
+        return x>= 0.15*self.width and x<= 0.15*self.width+2.5*self.cell_size and y>= self.height+self.height*0.075 and y <= self.height+self.height*0.075+1.5*self.cell_size
+    def __in_next_button_range(self,x,y):
+        return x>=0.82*self.width and x<=0.82*self.width+2*self.cell_size and y>=self.height+self.height*0.1 and y <= self.height+self.height*0.1+ self.cell_size
+    def reset_grid(self):
+        for row in range(self.grid_height):
+            for col in range(self.grid_width):
+                self.__draw_rect(self.RED, row, col)
     def update_grid(self):
         #self.screen.fill(self.RED)
         for row in range(self.grid_height):
@@ -49,11 +80,39 @@ class Gui():
                 self.on_exit_press()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = pygame.mouse.get_pos()
-                    # find each row and column that has that specific mouse click
-                    # finds specific squqare
-                row = y//self.cell_size
-                col = x//self.cell_size
-                self.on_cell_press(row, col)
+                if self.__in_grid_range(x,y):
+                        # find each row and column that has that specific mouse click
+                        # finds specific squqare
+                    row = y//self.cell_size
+                    col = x//self.cell_size
+                    self.on_cell_press(row, col)
+                if self.__in_start_button_range(x,y):
+                    started = True
+                    while started:
+                        self.on_next_gen_press()
+                        self.update()
+                        time.sleep(0.5)
+                        for event in pygame.event.get():
+                            if event.type == pygame.MOUSEBUTTONDOWN:
+                                z,z1 = pygame.mouse.get_pos()
+                                if self.__in_stop_button_range(z,z1):
+                                    started = False
+                                if self.__in_reset_button_range(z,z1):
+                                    started = False
+                                    self.on_reset()
+                                # if self.__in_next_button_range(z,z1):
+                                #     self.on_next_gen_press()
+                            if event.type == pygame.QUIT:
+                                started = False
+                                self.on_exit_press()
+                    
+                    #print('clicked start')
+                if self.__in_reset_button_range(x,y):
+                    self.on_reset()
+                # if self.__in_stop_button_range(x,y):
+                #     print('clicked stop')
+                if self.__in_next_button_range(x,y):
+                    self.on_next_gen_press()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     self.on_next_gen_press()
@@ -63,6 +122,9 @@ class Gui():
         self.__draw_grid(self.BLACK)
         pygame.display.flip()
     
-    
+    def draw_text(self,text,font,text_col,x,y):
+        img = font.render(text,True,text_col)
+        self.screen.blit(img, (x,y))
+
     def quit(self):
         pygame.quit()
