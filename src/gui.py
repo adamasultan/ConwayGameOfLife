a1daf32338ac4e0e6222778e1f92f1ee01196653
adamasultan <adamasultan@gmail.com>
2023-08-28 11:54:20 -0400

    split the drawing of buttons to seperate functions

--- src/gui.py
+++ src/gui.py
@@ -25,18 +25,24 @@ class Gui():
         self.screen.fill(self.RED)
         self.__draw_grid(self.BLACK)
         pygame.draw.rect(self.screen, self.GREY, ((0,self.height),(self.width, self.height)))
-        self.__draw_buttons()
+        self.__draw_start()
+        self.__draw_reset()
+        self.__draw_stop()
+        self.__draw_next() 
 
     def __draw_rect(self,color, row, col):
         pygame.draw.rect(self.screen, color, (col*self.cell_size, row*self.cell_size, self.cell_size, self.cell_size))
 
-    def __draw_buttons(self):
+    def __draw_start(self):
         pygame.draw.rect(self.screen, self.GREEN, ((0.4*self.width, self.height+self.height*0.05), (4*self.cell_size, 2*self.cell_size)))
         self.draw_text("Start", self.start_text_font, (0,0,0), 0.46*self.width, self.height+self.height*0.08)
+    def __draw_reset(self):
         pygame.draw.rect(self.screen, self.GREEN, ((0.15*self.width, self.height+self.height*0.075), (2.5*self.cell_size, 1.5*self.cell_size)))
         self.draw_text("Reset", self.reset_text_font, (0,0,0), 0.17*self.width, self.height+self.height*0.09)
+    def __draw_stop(self):
         pygame.draw.rect(self.screen, self.RED, ((0.65*self.width, self.height+self.height*0.1), (2*self.cell_size, self.cell_size)))
         self.draw_text("Stop", self.stop_and_next_text_font, (0,0,0), 0.68*self.width, self.height+self.height*0.11)
+    def __draw_next(self):
         pygame.draw.rect(self.screen, self.GREEN, ((0.82*self.width, self.height+self.height*0.1), (2*self.cell_size, self.cell_size)))
         self.draw_text("Next", self.stop_and_next_text_font, (0,0,0), 0.85*self.width, self.height+self.height*0.11)
     def __draw_grid(self, color):
